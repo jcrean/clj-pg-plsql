c8904f8c1a18447cce61be1952907ff978075454
Joshua Crean <joshua.crean@gmail.com>
2011-03-11 13:34:07 -0500

    adding basic dependencies for connecting to postgres

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
 (defproject clj-pg-plsql "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+  :description "Postgres function creation/execution via Clojure"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [com.relaynetwork/clorine "1.0.3"]
+                 [postgresql/postgresql "9.0-801.jdbc4"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]])
--- src/clj_pg_plsql/core.clj
+++ src/clj_pg_plsql/core.clj
@@ -1,1 +1,15 @@
-(ns clj-pg-plsql.core)
+(ns clj-pg-plsql.core
+  (:require [clojure.contrib.sql  :as sql]
+            [rn.clorine.core      :as cl]))
+
+(cl/register-connection! :test-db
+     {:driver-class-name "org.postgresql.Driver"
+      :user              "jcrean"
+      :password          ""
+      :url               "jdbc:postgresql://localhost:5432/jc_test"
+      })
+
+
+(cl/with-connection :test-db
+  true)
+
