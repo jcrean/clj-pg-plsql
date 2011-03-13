114d5a3c179f2eee6d739b75f98648b65e8c149d
Joshua Crean <joshua.crean@gmail.com>
2011-03-12 20:05:54 -0500

    adding defproc macro which declares a function in postgres and returns a fn that calls it

--- src/clj_pg_plsql/core.clj
+++ src/clj_pg_plsql/core.clj
@@ -11,5 +11,42 @@
 
 
 (cl/with-connection :test-db
-  true)
+  (sql/insert-rows :foobar
+                   [1 "thing" "thing2"]
+                   [2 "stuff" "more stuff"]))
+
+
+(defn create-or-replace [name]
+  (str "CREATE OR REPLACE FUNCTION " name "() RETURNS integer AS $$"))
+
+(defn begin []
+  "BEGIN ")
+
+(defn end []
+  "END; ")
+
+(defn close-func []
+  "$$ LANGUAGE plpgsql;")
+
+(defmacro defproc [name & body]
+  (cl/with-connection :test-db
+    (sql/do-commands
+     (str (create-or-replace name)
+          "DECLARE thing integer;"
+          (begin)
+          "SELECT id INTO thing FROM foobar WHERE attr1 = 'stuff';"
+          "RETURN thing;"
+          (end)
+          (close-func))))
+  (let [call-str (str "select " name "();")]
+    (def name (fn []
+                  (cl/with-connection :test-db
+                    (sql/with-query-results
+                      results
+                      [call-str]
+                      (doall results)))))))
+
+(defproc testing)
+
+(testing)
 
